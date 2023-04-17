c6ca653c15cd40d8dc1550a0aaa96232e96f9a2a
malachigamblin <malachi.gamblin@gmail.com>
2023-04-16 20:51:54 -0400

    set up functions to view all departments, roles and employees

--- server.js
+++ server.js
@@ -69,3 +69,41 @@ function introQuestion() {
       }
     });
 }
+
+function viewAllDepartments() {
+  const sql = `SELECT department.id, department.name AS Department FROM department;`;
+  db.query(sql, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.table(res);
+    introQuestion();
+  });
+}
+
+function viewAllRoles() {    
+    const sql = `SELECT role.id, role.title AS role,role.salary, department.name AS department FROM role INNER JOIN department ON (department.id = role.repartment_id)`;
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+        introQuestion();
+    })
+}
+
+
+function viewAllEmployess() {
+  const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;`;
+  db.query(sql, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.table(res);
+    introQuestion();
+  });
+}
+
